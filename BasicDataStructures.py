6d082f5fefd5b450a171660fe99432078e506581
Katya <86482797+Baytszy@users.noreply.github.com>
2024-06-27 06:43:04 +0300

    Add files via upload

---
+++ BasicDataStructures.py
@@ -0,0 +1,21 @@
+a = "мама мыла раму"
+print(len(a))
+first = 5
+second = 12
+summa = first + second
+diff = first - second
+print("5 + 12 =", summa)
+print("5 - 12 = ", diff)
+first_ = 3
+second_ = 5
+third = 7
+mean = (first_ + second_ + third) / 3
+print(mean)
+first_string = "Понедельник"
+second_string = "Вторник"
+print(first_string + ", "+ second_string)
+a_ = 4
+b_ = 7
+c_ = 3
+f = (((a_ * b_) + (a_ * c_))**3) / 2
+print(f)
